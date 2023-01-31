18a649c234084e929ebdd9a3cef0018021b32580
İlker Şelimen <ilkerselimen@gmail.com>
2023-01-31 22:26:07 +0300

    version 17

---
+++ src/main/java/advanced_practice/practice13/Aile.java
@@ -0,0 +1,13 @@
+package advanced_practice.practice13;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Aile {
+
+    private Map<Map<String,String>,Map<Integer, List<String>>> aile = new HashMap<>();
+
+
+
+}
---
+++ src/main/java/advanced_practice/practice13/Q02_Map_Aile.java
@@ -0,0 +1,22 @@
+package advanced_practice.practice13;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Q02_Map_Aile {
+    //Ad-Soyad-Yaş ve kardeşler listesini tek bir Map içerisinde saklayan bir kod yazınız.
+    public static void main(String[] args) {
+
+
+
+    }
+
+    //Map<String,String> adSoyad = new HashMap<>();
+    //        adSoyad.put("Ali","Can");
+    //
+    //        Map<Map<String,String>,Integer> adSoyadYas = new HashMap<>();
+    //        adSoyadYas.put(adSoyad,12);
+    //        System.out.println("adSoyadYas = " + adSoyadYas);// adSoyadYas = {{Ali=Can}=12}
+
+}
