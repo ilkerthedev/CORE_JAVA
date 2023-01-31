cc96247db56b7949f619b6c9f329d00ba2e963c4
İlker Şelimen <ilkerselimen@gmail.com>
2023-01-31 23:41:10 +0300

    version 17

--- src/main/java/advanced_practice/practice13/Q03_Enum_BeslenmeEgzersiz.java
+++ src/main/java/advanced_practice/practice13/Q03_Enum_BeslenmeEgzersiz.java
@@ -1,15 +1,24 @@
 package advanced_practice.practice13;
 
 public class Q03_Enum_BeslenmeEgzersiz {
-
     //Aylara göre sebze ve meyve tavsiyesi içeren ve toplam yapılması gereken egzersiz saatlerini söyleyen bir kod yazınız.
     //20 saat üstü egzersiz yapılacak ayları ekrana yazdırınız.
 
     public static void main(String[] args) {
 
-        System.out.println("BeslenmeEgzersiz.Ocak.toplamEgzersizSaati = " + BeslenmeEgzersiz.Ocak.toplamEgzersizSaati);
-        System.out.println(BeslenmeEgzersiz.Ekim.ayinMeyvesi);
+        System.out.print("20 saat üstü egzersiz yapılacak aylar: ");
+        for (BeslenmeEgzersiz w:BeslenmeEgzersiz.values()){
+            if (w.toplamEgzersizSaati>20){
+                System.out.print(w+", ");
+            }
+        }
+
     }
-    
+
+
+
+
+
+
 
 }
