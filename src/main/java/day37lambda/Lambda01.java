6b70033fba8809b66aeae0d6c28cc909a2954b8c
İlker Şelimen <ilkerselimen@gmail.com>
2023-02-03 21:46:49 +0300

    version 19

---
+++ src/main/java/day37lambda/Lambda01.java
@@ -0,0 +1,19 @@
+package day37lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lambda01 {
+
+    public static void main(String[] args) {
+
+        List<Integer> myList = new ArrayList<>();
+        myList.add(12);
+        myList.add(8);
+        myList.add(23);
+        myList.add(10);
+        myList.add(7);
+
+    }
+
+}
