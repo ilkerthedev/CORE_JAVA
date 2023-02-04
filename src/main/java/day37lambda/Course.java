0f24986269341f14d2fa6d528fcea69cf3af4beb
İlker Şelimen <ilkerselimen@gmail.com>
2023-02-04 23:58:44 +0300

    version 19

---
+++ src/main/java/day37lambda/Course.java
@@ -0,0 +1,63 @@
+package day37lambda;
+
+public class Course {
+
+    private String Season;
+    private String courseName;
+    private int averageScore;
+    private int numberOfStudents;
+
+    //1)Constructor without parameter
+    public Course() {
+    }
+
+    //2)Constructor with all parameters
+    public Course(String season, String courseName, int averageScore, int numberOfStudents) {
+        Season = season;
+        this.courseName = courseName;
+        this.averageScore = averageScore;
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    //3)getters() and setter()
+
+    public String getSeason() {
+        return Season;
+    }
+
+    public void setSeason(String season) {
+        Season = season;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(int averageScore) {
+        this.averageScore = averageScore;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    //4)toString()
+    @Override
+    public String toString() {
+        return "Course: Season=" + Season + ", courseName=" + courseName + ", averageScore=" + averageScore
+                + ", numberOfStudents=" + numberOfStudents;
+    }
+
+}
---
+++ src/main/java/day37lambda/Lambda03.java
@@ -0,0 +1,11 @@
+package day37lambda;
+
+public class Lambda03 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
